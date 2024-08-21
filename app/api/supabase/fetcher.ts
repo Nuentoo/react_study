a1d45875720c46f316e4133d92812fd295874c38
yusuke naito <yusuke03naito@gmail.com>
2024-08-21 18:06:25 +0900

    react_study : todo
    routeの変更_2

--- app/api/supabase/fetcher.ts
+++ app/api/supabase/fetcher.ts
@@ -3,7 +3,7 @@ import type { Tables, TablesInsert, TablesUpdate } from '@/app/api/supabase/sche
 type methodType = 'GET' | 'POST' | 'PATCH' | 'DELETE';
 
 const fetcherTodoTable = async (method: methodType, body?: TablesInsert<'todo Table'> | TablesUpdate<'todo Table'> | Tables<'todo Table'>['created_at']): Promise<Promise<Tables<'todo Table'>[]>> | never => {
-    const res = await fetch('http://localhost:3000/api/supabase', {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/supabase`, {
         method: method,
         headers: { 'Content-Type': 'application/json'},
         body: body ? JSON.stringify(body) : undefined,
