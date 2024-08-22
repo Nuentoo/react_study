a5404144892818cf245583231c59b0b92e50a3ca
yusuke naito <yusuke03naito@gmail.com>
2024-08-22 14:51:32 +0900

    react_study : initial
    Prettier 導入

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	compiler: {
-		// SSRでstyled-componentsが動作
-		styledComponents: true,
-	},
-	experimental: {
-		serverComponentsExternalPackages: ['@/_doc', '@/_old'],
-	},
-}
+  reactStrictMode: true,
+  compiler: {
+    // SSRでstyled-componentsが動作
+    styledComponents: true,
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['@/_doc', '@/_old'],
+  },
+};
 
 export default nextConfig;
