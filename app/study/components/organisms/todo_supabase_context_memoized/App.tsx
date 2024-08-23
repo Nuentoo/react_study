3e95300fe04708a85e7ef823a94bcb0e61657d5d
yusuke naito <yusuke03naito@gmail.com>
2024-08-23 12:02:55 +0900

    react_study : todo
    - test : TaskAppのreturnを静的にして検証_test

--- app/study/components/organisms/todo_supabase_context_memoized/App.tsx
+++ app/study/components/organisms/todo_supabase_context_memoized/App.tsx
@@ -8,10 +8,10 @@ export default async function TaskApp() {
   try {
     const todos = await fetcherTodoTable('GET');
     return (
-      <TasksProvider initialTodos={todos}>
-        <AddTodo />
-        <TaskList />
-      </TasksProvider>
+      <>
+        <strong>Data fetch failed ...</strong>
+        <TaskSkelton />
+      </>
     );
   } catch (error) {
     console.error('Error fetching initial todos:', error);
