10fc7e0e8e4ba5459d1889688e9bb9b0cf19e334
yusuke naito <yusuke03naito@gmail.com>
2024-08-23 12:16:11 +0900

    react_study : todo
    - test : formatDate関数実行だけさせてみる_test

--- app/study/components/organisms/todo_supabase_context_memoized/Task.tsx
+++ app/study/components/organisms/todo_supabase_context_memoized/Task.tsx
@@ -16,8 +16,8 @@ const MemoizedStyledRedButton = memo(StyledRedButton);
 const MemoizedStyledGreenButton = memo(StyledGreenButton);
 const MemoizedStyledInputText = memo(StyledInputText);
 
-const formatDate = (dateStr: string) => {
-  const date = new Date(dateStr);
+const formatDate = (() => {
+  const date = new Date('2024-07-08T06:52:11.530221+00:00');
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const day = String(date.getDate()).padStart(2, '0');
@@ -25,7 +25,7 @@ const formatDate = (dateStr: string) => {
   const minutes = String(date.getMinutes()).padStart(2, '0');
 
   return `${year}/${month}/${day} ${hours}:${minutes}`;
-};
+})();
 
 export default function Task({ created_at, done, id, title }: TodoSupabase) {
   // console.log(`${title}  再レンダリング???`)
