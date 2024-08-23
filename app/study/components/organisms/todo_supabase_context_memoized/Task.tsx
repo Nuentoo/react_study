14b9de5256cc1621cace380ab376984949e36472
yusuke naito <yusuke03naito@gmail.com>
2024-08-23 12:23:17 +0900

    react_study : todo
    - test : formatDate関数の返却値を文字列ベタ書き_test

--- app/study/components/organisms/todo_supabase_context_memoized/Task.tsx
+++ app/study/components/organisms/todo_supabase_context_memoized/Task.tsx
@@ -17,14 +17,7 @@ const MemoizedStyledGreenButton = memo(StyledGreenButton);
 const MemoizedStyledInputText = memo(StyledInputText);
 
 const formatDate = () => {
-  const date = new Date('2024-07-08T06:52:11.530221+00:00');
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const day = String(date.getDate()).padStart(2, '0');
-  const hours = String(date.getHours()).padStart(2, '0');
-  const minutes = String(date.getMinutes()).padStart(2, '0');
-
-  return `${year}/${month}/${day} ${hours}:${minutes}`;
+  return `2024/07/08 15:52`;
 };
 
 export default function Task({ created_at, done, id, title }: TodoSupabase) {
